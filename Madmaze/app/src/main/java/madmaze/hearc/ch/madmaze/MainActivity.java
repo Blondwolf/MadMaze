2abc3dd17d5cd45a885fd31e43fc5170c7aa5b03
Thomas Lovis <thomas.lovis@gmail.com>
2017-10-28 10:32:45 +0200

    add WifiPeerAdapter.java, create package wifi

--- Madmaze/app/src/main/java/madmaze/hearc/ch/madmaze/MainActivity.java
+++ Madmaze/app/src/main/java/madmaze/hearc/ch/madmaze/MainActivity.java
@@ -3,7 +3,10 @@ package madmaze.hearc.ch.madmaze;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,11 +15,23 @@ import madmaze.hearc.ch.madmaze.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    private IntentFilter intentFilter;
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
+        manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        channel = manager.initialize(this, getMainLooper(), null);
+*/
         loadFragment(HomeFragment.newInstance(), GameFragment.TAG);
     }
 
--- Madmaze/app/src/main/java/madmaze/hearc/ch/madmaze/dialogs/DeviceConnectionDialog.java
+++ Madmaze/app/src/main/java/madmaze/hearc/ch/madmaze/dialogs/DeviceConnectionDialog.java
@@ -5,10 +5,16 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.view.LayoutInflaterFactory;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import madmaze.hearc.ch.madmaze.R;
 
@@ -16,7 +22,25 @@ import madmaze.hearc.ch.madmaze.R;
  * Created by thomas on 27.10.2017.
  */
 
-public class DeviceConnectionDialog extends DialogFragment {
+public class DeviceConnectionDialog extends DialogFragment implements WifiP2pManager.ChannelListener, WifiP2pManager.PeerListListener {
+
+    private View view;
+
+    public DeviceConnectionDialog() {
+        super();
+    }
+/*
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.device_list, null);
+        return view;
+    }
+*/
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -37,5 +61,20 @@ public class DeviceConnectionDialog extends DialogFragment {
             }
         });
         return builder.create();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onChannelDisconnected() {
+
+    }
+
+    @Override
+    public void onPeersAvailable(WifiP2pDeviceList peers) {
+
     }
 }
